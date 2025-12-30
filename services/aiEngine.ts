b18f12ec559ff673a9ac205838922c1a847855cb
sonnyria <sonnyria@gmail.com>
2025-12-30 18:58:03 +0100

    Update aiEngine.ts

--- services/aiEngine.ts
+++ services/aiEngine.ts
@@ -93,11 +93,11 @@ export async function findMatches(date: string, league: string, country: string)
   try {
     const response = await ai.models.generateContent({
       //model: "gemini-3-flash-preview",
-      model: "gemini-1.5-flash",
+      model: "googleai/gemini-1.5-flash"
       contents: prompt,
       config: { 
         tools: [{ googleSearch: {} }], 
-       // responseMimeType: "application/json" 
+        responseMimeType: "application/json" 
       },
     });
 
